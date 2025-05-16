2998fee62d91a71c7279af4fb4f5da4dd088349f
victorien denoyelle <victorien.denoyelle@epitech.eu>
2025-05-16 13:12:42 +0200

    UPD on creation's files

---
+++ src/controleur/crea.ts
@@ -0,0 +1,26 @@
+import { vueCrea } from "./class_crea";
+
+vueCrea.init (
+    {
+        btnRetour       		: document.querySelector('[id=btnRetour]'),
+        btnvalider      		: document.querySelector('[id=btnValider]'),
+        btnSupprimerAdhe        : document.querySelector('[id=btnSupprimerAdhe]'),
+        btnModifierAdhe         : document.querySelector('[id=btnModifierAdhe]'),
+        btnAjouterAdhe          : document.querySelector('[id=btnAjouterAdhe]'),
+        num_adhé 				: document.querySelector('[id=num_adhé]'),
+        date_adhé 				: document.querySelector('[id=date_adhé]'),
+        comm					: document.querySelector('[id=comm]'),
+        num_ad					: document.querySelector('[id=num_ad]'),
+        table_abo			   	: document.querySelector('[id=table_abo]'),
+        total                   : document.querySelector('[id=total]'),
+        email                   : document.querySelector('[id=email]'),
+        adresse                 : document.querySelector('[id=adresse]'),
+        codeVille               : document.querySelector('[id=codeVille]'),
+        categorie               : document.querySelector('[id=categorie]'),
+        nom                     : document.querySelector('[id=nom]'),
+        abonnement_theme_edit   : document.querySelector('[id=abonnement_theme_edit]'),
+        papier                  : document.querySelector('[id=papier]'),
+        btnValidModif           : document.querySelector('[id=btnValidModif]'),
+        btnAnnulModif           : document.querySelector('[id=btnAnnulModif]'),
+        theme                   : document.querySelector('[id=theme]'),
+    } );
